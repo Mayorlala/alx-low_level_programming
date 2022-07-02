233f22c9ddf55036510d644e4d7c1352a61ec599
Lateef Sulaiman <lateefsulaiman2017@gmail.com>
2022-07-01 21:43:07 -0700

    6-print_numberz.c
    git commit -m 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each
+ *and print the last digit of number stored in the value
+ *Return : always 0
+ */
+int main(void)
+{
+        int ch;
+        for (ch = 48; ch <= 57; ch++)
+        {
+
+             putchar(ch);}
+        }
+        for (ch = 65; ch <= 90; ch++)
+        {
+                putchar(10);
+        }
+        putchar(10);
+
+        return (0);
+
