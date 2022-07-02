92c124c4ab9f4de8d1c0105797d6252f8be84a19
Lateef Sulaiman <lateefsulaiman2017@gmail.com>
2022-07-01 21:53:26 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each
+ *and print the last digit of number stored in the value
+ *Return : always 0
+ */
+int main(void)
+{
+        char ch;
+        for (ch = 'z'; ch >= 'a'; ch--)
+        {
+                
+             putchar(ch)
+        }
+	char ch;
+        for (ch = 'a'; ch <= 'z'; ch++)
+        {
+                putchar(ch);
+        }
+        putchar(10);
+
+        return (0);
+
