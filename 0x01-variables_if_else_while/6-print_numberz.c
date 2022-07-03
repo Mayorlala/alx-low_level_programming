b3a6bb6bfe7a9e34358e47b5ec8461375618daf0
Lateef Sulaiman <lateefsulaiman2017@gmail.com>
2022-07-03 07:26:41 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,23 +1,20 @@
+#include <stdlib.h>
 #include <stdio.h>
-
 /**
- *main -> assign a random number to the variable n each
- *and print the last digit of number stored in the value
- *Return : always 0
- */
+ *main - prints all single digit numbers of base 10 starting from 0
+ *followed by a new line
+ *
+ *Return: Always 0
+ **/
 int main(void)
 {
         int ch;
+
         for (ch = 48; ch <= 57; ch++)
         {
-
-             putchar(ch);}
-        }
-        for (ch = 65; ch <= 90; ch++)
-        {
-                putchar(10);
+        putchar(ch);
         }
         putchar(10);
 
         return (0);
-
+}
