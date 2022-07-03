d8e9dd427e80aef1c4c311293f946a9120dc01fd
Lateef Sulaiman <lateefsulaiman2017@gmail.com>
2022-07-03 07:19:43 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,24 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-
 /**
- *main -> assign a random number to the variable n each
- *and print the last digit of number stored in the value
- *Return : always 0
- */
+ *main - prints the alphabet in lowercase, followed by a new line
+ *
+ *Return: Always 0
+ **/
 int main(void)
 {
         char ch;
+
         for (ch = 'a'; ch <= 'z'; ch++)
         {
-                if (ch != 'q' && ch != 'e')
-		{
-			putchar(ch);
-		}		
+        if (ch != 'q' && ch != 'e')
+        putchar(ch);
         }
-        for (ch = 65; ch <= 90; ch++)
-        {
-                putchar(10);
-        }
+
         putchar(10);
 
         return (0);
